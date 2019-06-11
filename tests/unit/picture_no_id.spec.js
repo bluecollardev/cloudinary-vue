3caedf6bb46a0db19f2b018fb0a9d2b662909416
Amir Tocker <amir.tocker@cloudinary.com>
2019-06-11 15:13:36 +0300

    Remove redundant call to `sourcesOfPicture`

--- tests/unit/picture_no_id.spec.js
+++ tests/unit/picture_no_id.spec.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import { mount } from "@vue/test-utils";
 import CldPicture from "../../src/components/CldPicture.vue";
-import { sourcesOfPicture } from "./sourcesOfPicture";
 
 describe("CldPicture", () => {
   it("should render a picture with no sources if a publicId is not defined", async () => {
@@ -17,6 +16,6 @@ describe("CldPicture", () => {
     expect(picture.is("picture")).toBe(true);
     expect(picture.findAll("img").length).toBe(1);
     expect(picture.find("img").attributes("src")).toBe(undefined);
-    expect(sourcesOfPicture(picture)).toEqual({});
+    expect(picture.findAll("source")).toEqual({});
   });
 });
